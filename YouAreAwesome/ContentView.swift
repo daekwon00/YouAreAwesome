407004a0cae10dd09a0cd0204796f4caea4471ba
daekwon yoo <yoosw00@gmail.com>
2025-01-17 17:10:50 +0900

    swiftUI basic 따라하기

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var message = "I am A Programmmer"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+
+            Spacer()
+
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+
+                Button("Great") {
+                    message = "Great!"
+                }
+
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
+        .font(.title2)
+        .tint(.orange)
     }
 }
 
