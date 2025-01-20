d347add5f7bc28269d1ae69fd3f11be16f1fc369
daekwon yoo <yoosw00@gmail.com>
2025-01-20 15:14:15 +0900

    #16 ch. 1.9 Arrays 강의 구현

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var imageNumber = 0
+    @State private var messageNumber = 0
 
     var body: some View {
 
@@ -26,19 +27,36 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
 
             Spacer()
 
             Button("Show Message") {
-                let message1 = "You are Awesome!"
-                let message2 = "You are Great!"
+//                let message1 = "You are Awesome!"
+//                let message2 = "You are Great!"
+//                let message3 = "Fabulous? That's You!"
+//
+//                if message == message1 {
+//                    message = message2
+//                } else if message == message2 {
+//                    message = message3
+//                } else {
+//                    message = message1
+//                }
+                let messages = ["You are Awesome!",
+                               "You are Great!",
+                               "You Are Fantastic!",
+                               "Fabulous? That's You!",
+                                "You Make Me Smile!",
+                                "When the Genius Bar Needs Help, They Call You!"]
 
-                //TODO: - Update the image variable
-                message = (message == message1 ? message2 : message1)
-//                imageName = (imageName == "image0" ? "image1" : "image0")
+                message = messages[messageNumber]
+                messageNumber += 1
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
 
                 imageName = "image\(imageNumber)"
-//                imageNumber = imageNumber + 1
                 imageNumber += 1
 
                 if imageNumber > 9 {
